05aeff8c578cde3babce2b709e3ce20d92f39f10
Athiban-Odu-Pantham <athibanodu@student.tce.edu>
2023-09-09 15:47:28 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import Home from "./pages/home/Home";
+import Single from "./single/Single";
+import Write from "./pages/write/Write";
+import { Component } from "react";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import TopBar from "./components/topbar/TopBar";
+import React from "react";
+import { BrowserRouter as Router, Route,Routes, Link, BrowserRouter } from 'react-router-dom';
+import SinglePost2 from "./components/singlePost2/SinglePost2";
+import SinglePost from "./components/singlePost/SinglePost";
+import Singlepost3 from "./components/singlepost3/Singlepost3";
+
+function App() {
+  return (
+    <>
+    <br />
+    <Login/>
+    
+        </>
+
+  );
+}
+
+export default App;
+
+
+
+
---
+++ src/index.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import Home from './pages/home/Home';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import TopBar from './components/topbar/TopBar';
+import Write from './pages/write/Write';
+import Settings from './pages/settings/Settings';
+import Login from './pages/login/Login';
+import Register from './pages/register/Register';
+import Sidebar from './components/sidebar/Sidebar';
+import SinglePost from './components/singlePost/SinglePost';
+import SinglePost2 from './components/singlePost2/SinglePost2';
+import Singlepost3 from './components/singlepost3/Singlepost3';
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "home",
+    element: 
+    <>
+    <TopBar/>
+    <Home/>
+    </>
+  },
+  {
+    path: "write",
+    element: <>
+    <TopBar/>
+    <Write/>
+    </>
+  },
+  {
+    path: "settings",
+    element: <>
+    <TopBar/>
+    <Settings/>
+    </>
+  },
+  {
+    path: "logout",
+    element: <Login/>
+  },
+  {
+    path: "register",
+    element: 
+    <Register/>
+  },
+  {
+    path: "about",
+    element: 
+    <>
+    <TopBar/>
+    <Sidebar/>
+    </>
+  },
+  {
+    path: "post1",
+    element: 
+    <>
+    <TopBar/>
+    <SinglePost/>
+    </>
+  },
+  {
+    path: "post2",
+    element: 
+    <>
+    <TopBar/>
+    <SinglePost2/>
+    </>
+  },
+  {
+    path: "post3",
+    element: 
+    <>
+    <TopBar/>
+    <Singlepost3/>
+    </>
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <RouterProvider router={router} />
+);
+
+
